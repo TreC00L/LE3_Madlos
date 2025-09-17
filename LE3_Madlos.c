7fa1373241ddd81491d3646414cd2b2fe4e81ed0
TreC00L <tcmadlos66645@liceo.edu.ph>
2025-09-17 09:26:22 +0800

    bag o update

--- LE3_Madlos.c
+++ LE3_Madlos.c
@@ -4,76 +4,65 @@ int main(void)
 {
     char name[100];
     char section[50];
-    double num1, num2;
-    double sum, diff, prod, quot;
+    int n1, n2;
+    float sum, diff, prod, quot;
 
     printf("Enter Complete Name: ");
-
-    if (fgets(name, sizeof(name), stdin) == NULL)
-    {
-        fprintf(stderr, "Error reading name.\n");
-        return 1;
-    }
+    fgets(name, sizeof(name), stdin);
 
     {
-        size_t len = 0;
-        while (name[len] != '\0')
-            len++;
-        if (len > 0 && name[len - 1] == '\n')
+        int i = 0;
+        while (name[i] != '\0')
         {
-            name[len - 1] = '\0';
+            if (name[i] == '\n')
+            {
+                name[i] = '\0';
+                break;
+            }
+            i++;
         }
     }
 
     printf("Enter Section: ");
-    if (fgets(section, sizeof(section), stdin) == NULL)
-    {
-        fprintf(stderr, "Error reading section.\n");
-        return 1;
-    }
+    fgets(section, sizeof(section), stdin);
 
     {
-        size_t len = 0;
-        while (section[len] != '\0')
-            len++;
-        if (len > 0 && section[len - 1] == '\n')
+        int i = 0;
+        while (section[i] != '\0')
         {
-            section[len - 1] = '\0';
+            if (section[i] == '\n')
+            {
+                section[i] = '\0';
+                break;
+            }
+            i++;
         }
     }
 
     printf("\nEnter first number: ");
-    if (scanf("%lf", &num1) != 1)
-    {
-        fprintf(stderr, "Invalid input for first number.\n");
-        return 1;
-    }
+    scanf("%d", &n1);
 
     printf("Enter second number: ");
-    if (scanf("%lf", &num2) != 1)
-    {
-        fprintf(stderr, "Invalid input for second number.\n");
-        return 1;
-    }
+    scanf("%d", &n2);
 
-    sum = num1 + num2;
-    diff = num1 - num2;
-    prod = num1 * num2;
-    if (num2 != 0)
+    sum = n1 + n2;
+    diff = n1 - n2;
+    prod = n1 * n2;
+    if (n2 != 0)
     {
-        quot = num1 / num2;
+        quot = (float)n1 / n2;
     }
 
     printf("\nStudent Calculator\n");
     printf("Student name: %s\n", name);
     printf("Section: %s\n", section);
     printf("Results:\n");
-    printf("%.2f + %.2f = %.2f\n", num1, num2, sum);
-    printf("%.2f - %.2f = %.2f\n", num1, num2, diff);
-    printf("%.2f * %.2f = %.2f\n", num1, num2, prod);
-    if (num2 != 0)
+    printf("%d + %d = %.2f\n", n1, n2, sum);
+    printf("%d - %d = %.2f\n", n1, n2, diff);
+    printf("%d * %d = %.2f\n", n1, n2, prod);
+    if (n2 != 0)
     {
-        printf("%.2f / %.2f = %.2f\n", num1, num2, quot);
+        printf("%d / %d = %.2f\n", n1, n2, quot);
     }
     else
     {
